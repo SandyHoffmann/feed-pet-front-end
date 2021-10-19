abe5f6b4d6eb24ddafae68675c7996504a5b3413
Sandy Hoffmann <manglesan98@gmail.com>
2021-10-18 21:02:13 -0300

    arrumando

--- src/service/chat.js
+++ src/service/chat.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://feed-pet-chat.herokuapp.com", {autoConnect:false});
+export const socket = io("https://feed-pet-chat.herokuapp.com/", {autoConnect:false});
 
 socket.onAny((event,...args) => {
     console.log(event,args)
--- src/service/index.js
+++ src/service/index.js
@@ -3,7 +3,7 @@ import authServices from "./authServices";
 
 export const api = axios.create({
     baseURL:"https://feed-pet-back.herokuapp.com/",
-    timeout:1000
+    timeout:2000
 })
 
 api.interceptors.request.use(config => {
@@ -22,8 +22,12 @@ api.interceptors.response.use(function (response) {
   }, async function (error) {
       const originalRequest = error.config;
       const loginUrl = `https://feed-pet-back.herokuapp.com/auth/login`;
+      const homeUrl = `https://feed-pet-back.herokuapp.com/animais`;
       const refreshTokenUrl = "https://feed-pet-back.herokuapp.com/auth/refreshToken";    
-      if (error.response?.status === 401 && originalRequest.url !== refreshTokenUrl && error.request.responseURL !== loginUrl) {      
+      if (error.response?.status === 401
+        && originalRequest.url !== refreshTokenUrl
+        && error.request.responseURL !== loginUrl
+        && error.request.responseURL !== homeUrl) {      
         await authServices.refreshToken();     
         window.location.replace("/login"); 
         return api(originalRequest);
