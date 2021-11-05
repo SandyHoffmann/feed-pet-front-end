97f2fecf32872ab752495e4571e2629190d84787
Sandy Hoffmann <manglesan98@gmail.com>
2021-11-04 21:45:57 -0300

    Testando Timeout

--- src/service/index.js
+++ src/service/index.js
@@ -3,7 +3,7 @@ import authServices from "./authServices";
 
 export const api = axios.create({
     baseURL:"https://feed-pet-back.herokuapp.com/",
-    timeout:5000
+    timeout:20000
 })
 
 api.interceptors.request.use(config => {
